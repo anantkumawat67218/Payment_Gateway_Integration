e04fe5c631b4f17aba9471a18bcf0bfc3992b7af
anantkumawat67218 <84660908+anantkumawat67218@users.noreply.github.com>
2022-10-12 19:16:52 +0530

    Add files via upload

---
+++ indexJS.js
@@ -0,0 +1,14 @@
+(function () {
+    var d = document;
+    var x = !d.getElementById('razorpay-embed-btn-js');
+    if (x) {
+        
+        var s = d.createElement('script'); s.defer = !0; s.id = 'razorpay-embed-btn-js';
+        s.src = 'https://cdn.razorpay.com/static/embed_btn/bundle.js'; d.body.appendChild(s);
+    }
+    else {
+        var rzp = window['__rzp__'];
+        rzp && rzp.init && rzp.init();
+    }
+})();
+
